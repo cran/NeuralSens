89c6236a05ea91d9fc5c58ba1330226d6bbb31a8
Jaime Pizarroso Gonzalo <jpizarroso@alu.comillas.edu>
2019-06-04 11:00:03 +0000

    version 0.0.2

---
+++ R/NeuralSens-package.R
@@ -0,0 +1,8 @@
+#' NeuralSens: Sensibility analysis to NeuralNets
+#'
+#' Visualization and analysis tools to aid in the interpretation of
+#' neural network models.
+#'
+#' @name NeuralSens
+#' @docType package
+NULL
